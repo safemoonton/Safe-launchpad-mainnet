08fe64be40eb99da9e992627de75ef9fe9824078
Safemoon on TON <163474944+safemoonton@users.noreply.github.com>
2024-12-16 21:07:00 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,10 +20,10 @@ const Home = () => {
           experienced developer or a complete beginner.
         </p>
         <button
-          onClick={() => navigate("/safe-launch/create")}
+          onClick={() => navigate("/")}
           className="mt-6 lg:mt-36 py-3 sm:py-2 px-5 sm:px-6 rounded-2xl text-sm font-semibold dark:bg-white dark:hover:bg-white/20 text-safemoon-dark bg-white dark:hover:text-white hover:text-white hover:bg-white/20 dark:text-safemoon-dark"
         >
-          Launch Project
+          Launch Soon
         </button>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
@@ -37,10 +37,10 @@ const Home = () => {
               equitable distribution of tokens.
             </p>
             <button
-              onClick={() => navigate("/safe-launch")}
+              onClick={() => navigate("/")}
               className="py-3 sm:py-2 px-5 sm:px-6 rounded-2xl text-sm font-semibold dark:bg-white dark:hover:bg-white/20 text-safemoon-dark bg-white dark:hover:text-white hover:text-white hover:bg-white/20 dark:text-safemoon-dark"
             >
-              Check it out
+              Check soon
             </button>
           </div>
         </div>
