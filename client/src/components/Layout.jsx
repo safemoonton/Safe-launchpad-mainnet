718b6e7d82e2a7455928ab7c1d7874641a4496bf
Safemoon on TON <163474944+safemoonton@users.noreply.github.com>
2024-12-30 03:17:31 +0100

    Update Layout.jsx
    add riquestionline info beta

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -285,7 +285,12 @@ const Layout = ({ children }) => {
                       toggleMenu={() => setIsMenuOpen(false)}
                     />
                     <footer className="text-center text-xs my-4">
-                      <p>&copy; {currentYear} Safemoon Launchpad Beta</p>
+                      <p>&copy; {currentYear} Safemoon Launchpad Beta 
+                        <RiQuestionLine
+                          title="Aplikacja jest w wersji beta. Mogą występować błędy oraz problemy techniczne. Korzystając z aplikacji, zgadzasz się, że twórcy nie ponoszą odpowiedzialności za jakiekolwiek straty, w tym utratę środków."
+                          style={{ fontSize: "24px", cursor: "pointer" }}
+                        />
+                      </p>
                     </footer>
                   </div>
                 </div>
