b8ccfd0c6a5975a113600cd30d3cf56493b58dd9
Safemoon on TON <163474944+safemoonton@users.noreply.github.com>
2025-01-29 08:47:46 +0100

    Update CreateAirdrops.tsx

--- client/src/pages/CreateAirdrops.tsx
+++ client/src/pages/CreateAirdrops.tsx
@@ -410,7 +410,6 @@ const CreateAirdrops = () => {
                   name="title"
                   className="shadow appearance-none border rounded-2xl text-sm w-full py-2 pr-2 pl-4 text-gray-700 leading-tight focus:outline-none"
                   placeholder="Ex: Safe Moon Airdrop"
-                  placeholder="Ex: SafeMoon Airdrop"
                   required
                   value={formData.title}
                   onChange={handleInputChange}
